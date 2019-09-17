d6d0098cc5d54a1cf8aee488597e17e4318ec08f
GavinPeng <42169227+gavinpppeng@users.noreply.github.com>
2019-09-17 11:19:52 +1000

    Add files via upload

---
+++ Exams/mid/question_4.py
@@ -0,0 +1,59 @@
+'''
+Will be tested with a at equal equal to 2 and b at most equal to 10_000_000.
+'''
+    
+
+import sys
+from math import sqrt
+
+
+def f(a, b):
+    '''
+    >>> f(2, 2)
+    There is a unique prime number beween 2 and 2.
+    >>> f(2, 3)
+    There are 2 prime numbers between 2 and 3.
+    >>> f(2, 5)
+    There are 3 prime numbers between 2 and 5.
+    >>> f(4, 4)
+    There is no prime number beween 4 and 4.
+    >>> f(14, 16)
+    There is no prime number beween 14 and 16.
+    >>> f(3, 20)
+    There are 7 prime numbers between 3 and 20.
+    >>> f(100, 800)
+    There are 114 prime numbers between 100 and 800.
+    >>> f(123, 456789)
+    There are 38194 prime numbers between 123 and 456789.
+    '''
+    number_of_primes_at_most_equal_to_b = 0
+    # Insert your code here
+    if a < 2 or b > 10_000_000:
+        return False
+    L = prime_a_b(a,b)
+    number_of_primes_at_most_equal_to_b = len(L)
+    
+    if not number_of_primes_at_most_equal_to_b:
+        print(f'There is no prime number beween {a} and {b}.')
+    elif number_of_primes_at_most_equal_to_b == 1:
+        print(f'There is a unique prime number beween {a} and {b}.')
+    else:
+        print(f'There are {number_of_primes_at_most_equal_to_b} prime numbers between {a} and {b}.')
+
+def prime_a_b(a,b):
+    prime = [True] * (b+1)
+    prime_list = []
+    for i in range(2, round(sqrt(b))+1):
+        if prime[i]:
+            for j in range(i*i,b+1,i):
+                prime[j] = False
+
+    for k in range(a,b+1):
+        if prime[k]:
+            prime_list.append(prime[k])
+    return prime_list
+    
+
+if __name__ == '__main__':
+    import doctest
+    doctest.testmod()
